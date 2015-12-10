271ead40cd4f3fe05a7669d166abf4717d78046c
Nick <nfmcclure@gmail.com>
2015-12-10 10:52:16 -0800

    Update pycon_scraper.py

--- pycon_scraper.py
+++ pycon_scraper.py
@@ -3,6 +3,7 @@ from lxml import html
 import requests
 import json
 
+# Define the function to return JSON of information from year of pycon
 def get_pycon_info(year):
     pycon_base = 'https://us.pycon.org'
     pycon_contents = '/' + str(year) + '/schedule/talks/list'
@@ -11,6 +12,7 @@ def get_pycon_info(year):
     pycon_page = requests.get(pycon_base + pycon_contents)
     pycon_content_tree = html.fromstring(pycon_page.text)
 
+    # Create links to individual talks
     pycon_links = pycon_content_tree.xpath('/html/body/div/div/div[2]/div/div/div/div/h3/a/@href|'+\
                                            '//*[@id="content"]/div[2]/div/div/div/div/div/div/div/h3/a/@href')
 
@@ -20,15 +22,17 @@ def get_pycon_info(year):
     talk_category = []
     talk_description = []
 
-    counter = 1
+    counter = 1 # For printing purposes, probably better to use 'enumerate()' in loop
 
-    print('Found ' + str(len(pycon_links)) + ' talks.')
-
+    print('Found ' + str(len(pycon_links)) + ' talks.') # Print off how many talks found.
+    
+    # Loop through links, get all information.
     for link in pycon_links:
         print('Getting link ' + str(counter) + ' out of ' + str(len(pycon_links)))
         talk_html = pycon_base + link
         talk_page = requests.get(talk_html)
         talk_tree = html.fromstring(talk_page.text)
+        # Annoying XPath below (credit due to Chrome's 'inspect' function)
         talk_title1 = talk_tree.xpath('/html/body/div/div/div[2]/div/div/h2/text()|'+\
                                       '//*[@id="content"]/div[2]/div/div/div/div/div/h2/text()')
         talk_speaker1 = talk_tree.xpath('/html/body/div/div/div[2]/div/div/h4[2]/a/text()|'+\
@@ -57,8 +61,9 @@ def get_pycon_info(year):
     talk_data['level'] = talk_level
     talk_data['category'] = talk_category
     talk_data['description'] = talk_description
-    return(json.dumps(talk_data))
+    return(json.dumps(talk_data)) # Convert to JSON
 
 if __name__ == "__main__":
     year = 2014
     talk14_data = get_pycon_info(year)
+    # Probably need to add some way to write to file (SQL? CSV?)...
